c564fc11f57b49db9b579b1d3f8773ec38af5851
Russ Wilkie <russwilkie@gmail.com>
2018-01-21 17:41:44 -0600

    Added react http lab

---
+++ Labs/README.md
@@ -0,0 +1,8 @@
+# Lab Overview
+
+This lab works with Restful APIs and Axios request. At the end we programmed various GET and POST Request from
+servers and manipulating the data to build a blog.
+
+# How to use
+
+Replace src file's contents with one in a fully set-up React environment
---
+++ Labs/http/src/containers/Blog/Blog.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+//import axios from 'axios';
+import axios from '../../axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        post: [],
+        selectedPostId: null,
+        error: false
+    }
+    
+    componentDidMount() {
+        axios.get('/posts')
+                .then(response=>{
+                    const post = response.data.slice(0,4);
+                    const updatedPosts = post.map( post => {
+                        return{
+                                ...post,
+                                author: 'Mani'
+                            }
+                    });
+                    this.setState({post: updatedPosts});
+        })
+                .catch(error => {
+                    this.setState({error: true});
+        });
+    }
+    
+    postSelected = (id) => {
+        this.setState({selectedPostId: id});
+    }
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>
+        if(!this.state.error){
+         posts = this.state.post.map(post =>{
+            return <Post 
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelected(post.id)}
+            />
+        });
+    }
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
