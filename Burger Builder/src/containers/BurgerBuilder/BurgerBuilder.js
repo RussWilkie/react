a08c1f70bc65dff8c10c96dce94f56ee02189d54
Russ Wilkie <russwilkie@gmail.com>
2018-02-20 21:04:42 -0600

    Added routing to burger-builder

--- Burger Builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger Builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,28 +83,17 @@ class BurgerBuilder extends Component {
     }
     
     continueCheckout = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Manido Wilkie',
-                address: {
-                    street: '123 Fake Stree',
-                    zipCode: '12345',
-                    country: 'Springfield'
-                },
-                email: 'test@mail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-                .then(response=> {
-                    this.setState({loading: false, checkedout: false});
-        })
-                .catch(error =>{ 
-                    this.setState({loading: false, checkedout: false});
-        });
+      const queryParams = [];
+      for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+     }
+     queryParams.push('price=' + this.state.totalPrice);
+     const queryString = queryParams.join('&');
+      this.props.history.push({
+          pathname: '/checkout',
+          search: '?' + queryString
+        });  
+
     }
     render(){
         const disabledInfo = {
